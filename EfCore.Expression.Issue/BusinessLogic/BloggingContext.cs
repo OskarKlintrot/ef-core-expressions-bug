ac8ac6baf32a93eaf876ceb95dfbc1dbd2ecaaad
Oskar Klintrot <oskar.klintrot@p-bolaget.goteborg.se>
2019-06-26 09:09:31 +0200

    refactor: Put each class in it's own file

--- EfCore.Expression.Issue/BusinessLogic/BloggingContext.cs
+++ EfCore.Expression.Issue/BusinessLogic/BloggingContext.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using static Util.IntervalExpressionFactory;
 
 namespace BusinessLogic
 {
@@ -41,28 +37,5 @@ namespace BusinessLogic
                                    level: LogLevel.Debug))
                     .BuildServiceProvider()
                     .GetService<ILoggerFactory>();
-    }
-
-    public class Blog
-    {
-        public int BlogId { get; set; }
-        public string Url { get; set; }
-
-        public ICollection<Post> Posts { get; set; }
-    }
-
-    public class Post
-    {
-        public int PostId { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public DateTimeOffset Created { get; set; }
-        public DateTimeOffset Publish { get; set; }
-
-        public static Expression<Func<Post, bool>> GenerateIsNotYetPublishedExpression(DateTimeOffset dateTimeOffset)
-            => GenerateIntervalContainsExpression<Post>(dateTimeOffset, startDateExp: x => x.Created, endDateExp: x => x.Publish);
-
-        public int BlogId { get; set; }
-        public Blog Blog { get; set; }
     }
 }
---
+++ EfCore.Expression.Issue/BusinessLogic/Post.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using static Util.IntervalExpressionFactory;
+
+namespace BusinessLogic
+{
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset Publish { get; set; }
+
+        public static Expression<Func<Post, bool>> GenerateIsNotYetPublishedExpression(DateTimeOffset dateTimeOffset)
+            => GenerateIntervalContainsExpression<Post>(dateTimeOffset, startDateExp: x => x.Created, endDateExp: x => x.Publish);
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
